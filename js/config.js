e029e1e29f4cd65b0d519990b6d7b0c81293915c
CGrant10 <87659697+CGrant10@users.noreply.github.com>
2026-08-14 22:18:03 -0500

    Bump app version to 1.70.2

--- js/config.js
+++ js/config.js
@@ -4,4 +4,4 @@ export const SUPABASE_ANON_KEY = "sb_publishable_I14JPQNBW-fhIwZ7WkBlAg_M9zolGNL
 export const LEAGUE_NAME = "DFL HQ";
 export const LEAGUE_FOUNDED = 2017;
 export const FIRST_SYNCED_SEASON = 2019;
-export const APP_VERSION = "1.70.1";
+export const APP_VERSION = "1.70.2";
