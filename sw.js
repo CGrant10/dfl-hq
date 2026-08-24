8b21b07d9e3074faa77b3d1e516029467457908e
CGrant10 <87659697+CGrant10@users.noreply.github.com>
2026-08-23 21:23:29 -0500

    Refine card gradients and recover profile unlock (#136)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // DFL HQ service worker
-const CACHE_NAME = "dfl-hq-v1.155.0";
+const CACHE_NAME = "dfl-hq-v1.156.0";
 const CDN_HOSTS = new Set(["cdn.jsdelivr.net","fonts.googleapis.com","fonts.gstatic.com","a.espncdn.com"]);
 const APP_SHELL = [
   "./","./index.html","./manifest.json",
